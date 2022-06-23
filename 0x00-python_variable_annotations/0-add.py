4791cb4609fe0aeebacfe1a86451dafe8971f4cd
homemix <kenmutati@gmil.com>
2022-06-23 09:43:50 +0300

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """
+    Add two numbers
+    :param a: number 1
+    :type a: float
+    :param b: number 2
+    :type b: float
+    :return: sum of a and b
+    :rtype: float
+    """
+    return a + b
