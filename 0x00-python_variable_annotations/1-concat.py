c31fed8f158e136d6dc92020d4e8ca10d52f0e11
homemix <kenmutati@gmil.com>
2022-06-23 12:53:30 +0300

    1. Basic annotations - concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Concatenate two strings
+"""
+
+
 def concat(str1: str, str2: str) -> str:
     """
     Concatenate two strings :param str1: string 1 :param str2: string 2
