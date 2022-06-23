a604c6850f1afe23b48026f204c3ccf06f7cf348
homemix <kenmutati@gmil.com>
2022-06-23 12:58:44 +0300

    2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+:return: the floor of a number
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Floor a number :param n: number
+    """
+    return math.floor(n)
