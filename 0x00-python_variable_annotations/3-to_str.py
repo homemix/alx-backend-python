8f86893bdb61cdb665ea8c5aa950a5a66b9a1a21
homemix <kenmutati@gmil.com>
2022-06-23 13:02:32 +0300

    3. Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+:return the string representation of a number
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    :return: the string representation of a number
+    """
+    return str(n)
