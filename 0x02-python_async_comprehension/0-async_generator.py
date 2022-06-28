464b3ac45c58f9f686f4afe556d904845fa1e657
homemix <kenmutati@gmil.com>
2022-06-28 14:00:31 +0300

    0. Async Generator - fix errors

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float,None,None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     :returns a random number between 0 and 10
     :return:
