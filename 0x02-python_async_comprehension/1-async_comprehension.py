6422f992b084bc51dd88ebf6c177525c50bb3485
homemix <kenmutati@gmil.com>
2022-06-28 14:17:01 +0300

    1. Async Comprehensions - fix return type ommision error

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import asyncio
+from typing import List
+
 """
 :returns async generator for a random number between 0 and 10
 """
@@ -6,7 +9,7 @@
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     :returns async generator for a random number between 0 and 10
     :return:
