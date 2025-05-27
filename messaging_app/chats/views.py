91f2d564e7b48566c091a0636d7fbe854cdf49c4
homemix <kenmutati@gmail.com>
2025-05-27 12:54:00 +0300

    EDIT: DRF views to include models create.

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters, status, viewsets
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
@@ -5,19 +6,29 @@ from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 
-class ConversationViewSet(ViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['conversation_id']
 
-    def list(self, request):
-        # conversations = Conversation.objects.all()
-        serializer = ConversationSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            conversation = serializer.save()
+            return Response(conversation.conversation_id, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MessageViewSet(ViewSet):
     queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['sent_at']
 
-    def list(self, request):
-        # messages = Message.objects.all()
-        serializer = MessageSerializer(self.queryset, many=True)
-        return Response(serializer.data)
+    def create(self, request, *args, **kwargs):
+        serializer = MessageSerializer(data=request.data)
+        if serializer.is_valid():
+            message = serializer.save()
+            return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
