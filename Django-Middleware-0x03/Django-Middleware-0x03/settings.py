9759b813a0248920112336f9e4c07096cfd37819
homemix <kenmutati@gmail.com>
2025-06-10 10:29:28 +0300

    ADD: 2. Restrict Chat Access by time

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 
     # Custom middleware
     "chats.middleware.RequestLoggingMiddleware",
-    'django.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 ROOT_URLCONF = 'Django-Middleware-0x03.urls'
